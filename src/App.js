a2844c8deefdcd0a36d32530e5bc8b2367579816
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-04-27 12:29:03 +0100

    Mobile Top Up page done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //import pages
-import OnboardingScreen from './Pages/OnboardingScreen.js';
-import Home from './Pages/Home.js';
-import Upgrade from './Pages/Upgrade.js';
-import Test from './Pages/Test.js';
-import WalletAd from './Pages/WalletAd.js';
-import Wallet from './Pages/Wallet.js';
-import Login from './Pages/Login.js';
-import Withdrawal from './Pages/Withdrawal.js';
-import Verification from './Pages/Verification.js';
-import MiningWallet from './Pages/MiningWallet.js';
+import OnboardingScreen from "./Pages/OnboardingScreen.js";
+import Home from "./Pages/Home.js";
+import Upgrade from "./Pages/Upgrade.js";
+import Test from "./Pages/Test.js";
+import WalletAd from "./Pages/WalletAd.js";
+import Wallet from "./Pages/Wallet.js";
+import Login from "./Pages/Login.js";
+import Withdrawal from "./Pages/Withdrawal.js";
+import Verification from "./Pages/Verification.js";
+import MiningWallet from "./Pages/MiningWallet.js";
 
 // import Components
-import Setting from './Component/Setting.js';
-import Switcher from './Component/Switcher';
-import Slide from './Component/Slide';
-import SendRecieveDrawer from './Component/SendRecieveDrawer'
-import ShareQRcode from './Component/ShareQRcode.js';
-import WithdrawSuccess from './Component/WithdrawSuccess.js';
-import Nav from './Component/Nav.js';
-
-
-
+import Setting from "./Component/Setting.js";
+import Switcher from "./Component/Switcher";
+import Slide from "./Component/Slide";
+import SendRecieveDrawer from "./Component/SendRecieveDrawer";
+import ShareQRcode from "./Component/ShareQRcode.js";
+import WithdrawSuccess from "./Component/WithdrawSuccess.js";
+import Nav from "./Component/Nav.js";
+import MobileTopUp from "./Pages/MobileTopUp.js";
 
 const App = () => {
-  return(
+  return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/">
-           {/* <OnboardingScreen /> */}
+            {/* <OnboardingScreen /> */}
             {/* <Upgrade /> */}
             {/* <Home /> */}
             {/* <Test /> */}
@@ -48,11 +46,12 @@ const App = () => {
             {/* <Login /> */}
             {/* <Verification /> */}
             <MiningWallet />
+            {/* <MobileTopUp /> */}
           </Route>
         </Switch>
       </div>
     </Router>
-  )
+  );
 };
 
 export default App;
